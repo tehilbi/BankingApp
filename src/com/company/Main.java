3b39494a246c1766048eb5257b80c3dbfc8e8dd4
tehilbi <tehilabitarov@gmail.com>
2020-05-18 10:57:08 +0300

    add BankAccount class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,43 @@
 package com.company;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a character");
-//        int a = scanner.nextInt();
-        char c = scanner.next().charAt(0);
-        System.out.println("value = " + c);
+
+    }
+}
+
+class BankAccount {
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    void deposit(int amount) {
+        if (amount != 0) {
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void withdraw(int amount) {
+        if (amount != 0) {
+            balance = balance - amount;
+            previousTransaction = -amount;
+        }
+    }
+
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("Withdrawn: " + Math.abs(previousTransaction));
+        } else {
+            System.out.println("No transaction occurred");
+        }
+    }
+
+    void showMenu() {
+
     }
 }
